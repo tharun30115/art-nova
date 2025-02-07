dc3c6111f96b9774c8dd5a164955594d0e8ee504
Tharun <tharungowda30115@gmail.com>
2025-02-07 23:56:51 +0530

    completed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const navbar = document.querySelector("[data-navbar]");
 const navToggler = document.querySelector("[data-nav-toggler]");
 
+
 navToggler.addEventListener("click", function () {
   navbar.classList.toggle("active");
   this.classList.toggle("active");
